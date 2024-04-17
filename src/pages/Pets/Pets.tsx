706f23b13a706f264d685a704dc9de8b6458424b
nozellabrenda <brenda.nozella@gmail.com>
2024-04-16 22:21:47 -0300

    Feat: Grid

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,3 +1,15 @@
+import { Link } from 'react-router-dom'
+import { Header } from '../../components/common/Header'
+import { Grid } from '../../components/layout/Grid/Grid'
+import styles from './Pets.module.css'
+
 export function Pets() {
-    return <></>
+    return (
+        <Grid>
+            <div className={StyleSheet.container}>
+                <Header />
+                <Link to="/pets/20">Ir para listagem</Link>
+            </div>
+        </Grid>
+    )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from './pages/Home'
+import { Pets } from "./pages/Pets";
+import { PetDetails } from "./pages/PetDetails/PetDetails";
 
 const router = createBrowserRouter([
     {
@@ -8,7 +10,16 @@ const router = createBrowserRouter([
     },
     {
     path: '/pets',
-    element: <Pets />,
+    children: [
+        {
+            index: true,
+            element: <Pets />
+        },
+        {
+            path: 'pets/:id',
+            element: <PetDetails />,
+        },
+    ],
     },
     {
     path: '/admin',
