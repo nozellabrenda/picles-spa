5f006558a8b88275ae1a85f51f0cf470e1588c03
nozellabrenda <brenda.nozella@gmail.com>
2024-04-23 11:00:34 -0300

    Feat: Searching by form filters and fixes

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,66 +1,104 @@
-interface IFilterColumns {
-  name: 'gender' | 'size' | 'type'
-  title: string
-  options: { value: string; text: string }[]
-}
+import { useSearchParams } from 'react-router-dom'
+import { Card } from '../../components/common/Card'
+import { Header } from '../../components/common/Header'
+import { Pagination } from '../../components/common/Pagination'
+import { Skeleton } from '../../components/common/Skeleton'
+import { Grid } from '../../components/layout/Grid'
+import { usePetList } from '../../hooks/usePetList'
+import styles from './Pets.module.css'
+import { Select } from '../../components/common/Select'
+import { Button } from '../../components/common/Button'
+import { filterColumns } from './Pets.constants'
+import { FormEvent } from 'react'
+import { GetPetsRequest } from '../../interfaces/pet'
 
-export const filterColumns: IFilterColumns[] = [
-  {
-    name: 'type',
-    title: 'Espécie',
-    options: [
-      {
-        value: '',
-        text: 'Todos',
-      },
-      {
-        value: 'cachorro',
-        text: 'Cachorros',
-      },
-      {
-        value: 'gato',
-        text: 'Gatos',
-      },
-    ],
-  },
-  {
-    name: 'size',
-    title: 'Porte',
-    options: [
-      {
-        value: '',
-        text: 'Todos',
-      },
-      {
-        value: 'pequeno',
-        text: 'Pequeno',
-      },
-      {
-        value: 'medio',
-        text: 'Médio',
-      },
-      {
-        value: 'grande',
-        text: 'Grande',
-      },
-    ],
-  },
-  {
-    name: 'gender',
-    title: 'Sexo',
-    options: [
-      {
-        value: '',
-        text: 'Todos',
-      },
-      {
-        value: 'femea',
-        text: 'Fêmea',
-      },
-      {
-        value: 'macho',
-        text: 'Macho',
-      },
-    ],
-  },
-]
+export function Pets() {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const urlParams = {
+    page: searchParams.get('page') ? Number(searchParams.get('page')) : 1,
+    type: searchParams.get('type') ?? '',
+    size: searchParams.get('size') ?? '',
+    gender: searchParams.get('gender') ?? '',
+  }
+
+  const { data, isLoading } = usePetList(urlParams)
+
+  function changePage(page: number) {
+    setSearchParams((params) => {
+      params.set('page', String(page))
+      return params
+    })
+  }
+
+  function getFormValue(form: HTMLFormElement) {
+    const formData = new FormData(form)
+    return Object.fromEntries(formData)
+  }
+
+  function updateSearchParams(urlParams: GetPetsRequest) {
+    const fields: (keyof GetPetsRequest)[] = ['type', 'size', 'gender']
+    const newParams = new URLSearchParams()
+
+    fields.forEach((field) => {
+      if (urlParams[field]) {
+        newParams.set(field, String(urlParams[field]))
+      }
+    })
+    newParams.set('page', '1')
+
+    return newParams
+  }
+
+  function applyFilters(event: FormEvent) {
+    event.preventDefault()
+
+    const formValues = getFormValue(event.target as HTMLFormElement)
+    const newSearchParams = updateSearchParams(formValues)
+
+    setSearchParams(newSearchParams)
+  }
+
+  return (
+    <Grid>
+      <div className={styles.container}>
+        <Header />
+        <form className={styles.filters} onSubmit={applyFilters}>
+          <div className={styles.columns}>
+            {filterColumns.map((filter) => (
+              <div key={filter.name} className={styles.column}>
+                <Select
+                  label={filter.title}
+                  defaultValue={urlParams[filter.name]}
+                  name={filter.name}
+                  options={filter.options}
+                />
+              </div>
+            ))}
+          </div>
+          <Button type="submit">Buscar</Button>
+        </form>
+        {isLoading && (
+          <Skeleton containerClassName={styles.skeleton} count={10} />
+        )}
+        <main className={styles.list}>
+          {data?.items?.map((pet) => (
+            <Card
+              key={pet.id}
+              href={`/pet/${pet.id}`}
+              text={pet.name}
+              thumb={pet.photo}
+            />
+          ))}
+        </main>
+        {data?.currentPage && (
+          <Pagination
+            currentPage={data.currentPage}
+            totalPages={data.totalPages}
+            onPageChange={(number) => changePage(number)}
+          />
+        )}
+      </div>
+    </Grid>
+  )
+}
