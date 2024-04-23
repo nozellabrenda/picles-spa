0b4b71c9360e2466601537cc1c83ad1b61344763
nozellabrenda <brenda.nozella@gmail.com>
2024-04-23 10:55:55 -0300

    Feat: pet listing with pagination

--- src/hooks/usePetList.tsx
+++ src/hooks/usePetList.tsx
@@ -1,17 +1,17 @@
-import { useQuery } from "@tanstack/react-query";
-import { getPets } from "../services/pets/getPets";
-import { GetPetsRequest } from "../interfaces/pet";
+import { useQuery } from '@tanstack/react-query'
+import { getPets } from '../services/pets/getPets'
+import { GetPetsRequest, GetPetsResponse } from '../interfaces/pet'
 
 interface IUsePetList {
-    data?: GetPetsRequest
-    isLoading: boolean
+  data?: GetPetsResponse
+  isLoading: boolean
 }
 
 export function usePetList(params: GetPetsRequest): IUsePetList {
-    const { data, isLoading } = useQuery({
-        queryKey: ['get-pets', params],
-        queryFn: () => getPets(params),
-    })
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-pets', params],
+    queryFn: () => getPets(params),
+  })
 
-    return { data, isLoading }
+  return { data, isLoading }
 }
