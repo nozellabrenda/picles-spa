f675e2089e58b21447e6debad4b2879254389085
nozellabrenda <brenda.nozella@gmail.com>
2024-04-24 21:25:01 -0300

    Feat: Shelter form w validation

--- src/routes.tsx
+++ src/routes.tsx
@@ -8,41 +8,41 @@ import { AuthHOC } from "./components/common/AuthHOC/AuthHOC";
 
 const router = createBrowserRouter([
     {
-    path: '/',
-    element: <Home />,
+        path: '/',
+        element: <Home />,
     },
     {
-    path: '/pets',
-    children: [
-        {
-            index: true,
-            element: <Pets />
-        },
-        {
-            path: 'pets/:id',
-            element: <PetDetails />,
-        },
-    ],
+        path: '/pets',
+        children: [
+            {
+                index: true,
+                element: <Pets />
+            },
+            {
+                path: 'pets/:id',
+                element: <PetDetails />,
+            },
+        ],
     },
     {
-    path: '/admin',
-    children: [
-        {
-            index: true,
-            element: <Shelter/>
-        },
-        {
-            path: '/admin/pets',
-            element: <AuthHOC />,
-            children: [
-                {
-                    index: true,
-                    element: <PetList/>
-                },
-            ],
-        }
-    ],
-    
+        path: '/admin',
+        children: [
+            {
+                index: true,
+                element: <Shelter />
+            },
+            {
+                path: '/admin/pets',
+                element: <AuthHOC />,
+                children: [
+                    {
+                        index: true,
+                        element: <PetList />
+                    },
+                ],
+            }
+        ],
+
     },
 ])
 
